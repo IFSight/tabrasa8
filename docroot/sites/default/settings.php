fc6c42a9404aef75308a01cf9fd9983394a65bd6
Kenn Herman <kenn@ifsight.com>
2017-02-24 10:33:23 -0800

    update fulcrum settings.php magic

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -718,10 +718,9 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 #   include __DIR__ . '/settings.local.php';
 # }
 
-if ($ff = '/fulcrum/fulcrum.php' AND file_exists($ff) AND (include $ff) AND isset($_FULCRUM)) {
-  fulcrum_cfg('pre', $_FULCRUM['conf'], $settings, $databases);
-} else if ($ff = "{$_SERVER['HOME']}/fulcrum/etc/fulcrum/php/fulcrum.php" AND file_exists($ff) AND (include $ff) AND isset($_FULCRUM)) {
-  fulcrum_cfg('pre', $_FULCRUM['conf'], $settings, $databases);
-} else if ($ff = "{$_SERVER['HOME']}/fulcrum/php/fulcrum.php" AND file_exists($ff) AND (include $ff) AND isset($_FULCRUM)) {
-  fulcrum_cfg('pre', $_FULCRUM['conf'], $settings, $databases);
+foreach (array('/fulcrum/fulcrum.php', "{$_SERVER['HOME']}/fulcrum/etc/fulcrum/php/fulcrum.php", "{$_SERVER['HOME']}/fulcrum/php/fulcrum.php") as $f) {
+  if (is_file($f) && (include $f) && isset($_FULCRUM)) {
+    fulcrum_cfg('pre', $_FULCRUM['conf'], $settings, $databases);
+    break;
+  }
 }
