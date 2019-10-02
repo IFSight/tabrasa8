9bd0f13a421d21c51c27983ff7dc1d64ad13047e
Rosemary Foley <rosemary@ifsight.com>
2019-10-02 16:41:38 -0400

    basic readme

---
+++ docroot/README.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Update and deployment information for the site.
+ */
+
+/**
+ * This site follows the standard deployment procedures.
+ * For now please refer to the Drupal Security Updates section of the
+ * Workflow document: https://docs.google.com/document/d/1-is4hJYILKahz4TASjpOq0Dc-8uhDNg6bMKaa6wZkxU/edit#heading=h.9w3hu83kwvdp
+ */
