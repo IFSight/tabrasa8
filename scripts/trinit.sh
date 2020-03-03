c7a12251905953cb324f9008a1499974283e9ff2
Lisa Ridley <lisa@ifsight.com>
2020-03-03 04:06:41 -0500

    updating post install script

--- scripts/trinit.sh
+++ scripts/trinit.sh
@@ -3,18 +3,16 @@
 # Initialization script for TabRasa8 project
 
 # Copy the settings.php file to docroot/sites/default/settings.php
-chmod 0777 ./docroot/sites/default/settings.php
 if [ ! -f "docroot/sites/default/settings.php" ]; then
 	cp ./drupal/settings.php ./docroot/sites/default/settings.php
+	chmod 0644 ./docroot/sites/default/settings.php
 fi
-chmod 0644 ./docroot/sites/default/settings.php
 
 # Copy the settings.local.php file to docroot/sites/default/settings.php
-chmod 0777 ./docroot/sites/default/settings.local.php
 if [ ! -f "docroot/sites/default/settings.local.php" ]; then
 	cp ./drupal/settings.local.php ./docroot/sites/default/settings.local.php
+	chmod 0644 ./docroot/sites/default/settings.local.php
 fi
-chmod 0644 ./docroot/sites/default/settings.local.php
 
 # Create the custom modules and themes directories
 mkdir -p ./docroot/modules/custom
