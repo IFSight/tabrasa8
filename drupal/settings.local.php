336a20d34c9c8f530bd78000634851244fb8b2fd
Lisa Ridley <lisa@ifsight.com>
2020-03-02 19:25:44 -0500

    Adding changes to composer.json, post install script, and settings.php / settings.local.php

---
+++ drupal/settings.local.php
@@ -0,0 +1,7 @@
+<?php
+
+$settings['hash_salt'] = '9WoEKsvXT5VxL96^%dGFbEeaG%2R92$gVaX3AkIqq&Fx^&xC28';
+
+$settings['trusted_host_patterns'] = [
+  '^tabrasa8\.ifdev$',
+];
--- scripts/trinit.sh
+++ scripts/trinit.sh
@@ -3,15 +3,30 @@
 # Initialization script for TabRasa8 project
 
 chmod 0777 ./docroot/sites/default/settings.php
-cp ./scripts/settings.php ./docroot/sites/default/settings.php
+if [ ! -f "docroot/sites/default/settings.php" ]; then
+	cp ./drupal/settings.php ./docroot/sites/default/settings.php
+fi
 chmod 0644 ./docroot/sites/default/settings.php
+
+chmod 0777 ./docroot/sites/default/settings.local.php
+if [ ! -f "docroot/sites/default/settings.local.php" ]; then
+	cp ./drupal/settings.local.php ./docroot/sites/default/settings.local.php
+fi
+chmod 0644 ./docroot/sites/default/settings.local.php
+
 mkdir -p ./docroot/modules/custom
 touch ./docroot/modules/custom/.gitkeep
 mkdir -p ./docroot/themes/custom
 touch ./docroot/themes/custom/.gitkeep
 
 # Remove all .git repo directories from vendor folder
-find ./vendor -name .git -type d -prune
+if [ -d "docroot/libraries/plupload" ]; then
+	rm -Rf docroot/libraries/plupload/examples
+fi
 
+if [ ! -d "docroot/sites/default/files/private" ]; then
+	mkdir -p docroot/sites/default/files/private
+	cp ./drupal/htaccess.private ./docroot/sites/default/files/private/.htaccess
+fi
 # and from Docroot
 find ./docroot -name .git -type d -prune
