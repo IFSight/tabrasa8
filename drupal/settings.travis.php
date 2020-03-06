5ce1a587598502fd73a7e376e273a45a845ea8ab
Lisa Ridley <lisa@ifsight.com>
2020-03-06 12:47:22 -0500

    Add settings.travis.php to project

---
+++ drupal/settings.travis.php
@@ -0,0 +1,18 @@
+<?php
+
+$settings['hash_salt'] = '9WoEKsvXT5VxL96^%dGFbEeaG%2R92$gVaX3AkIqq&Fx^&xC28';
+
+$settings['trusted_host_patterns'] = [
+  '^tabrasa8\.ifdev$',
+];
+
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'drupal',
+  'host' => 'localhost',
+  'port' => '3306',
+  'driver' => 'mysql',
+  'prefix' => '',
+  'collation' => 'utf8mb4_general_ci',
+ );
